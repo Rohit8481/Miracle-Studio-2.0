fd47957d24d23c4cba1f5a06ab5a55e0b37a1ee3
Rohit Kumar Pandit <singerrohit02@gmail.com>
2021-03-19 21:58:37 +0530

    1.1.3v

--- js/pre.js
+++ js/pre.js
@@ -6,3 +6,28 @@ window.addEventListener("load", vanish);
 function vanish() {
     loader.classList.add("disppear");
 }
+
+
+// slideshow loader js 
+
+var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+slideshows.forEach(initSlideShow);
+
+function initSlideShow(slideshow) {
+
+    var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
+
+    var index = 0,
+        time = 4000;
+    slides[index].classList.add('active');
+
+    setInterval(() => {
+        slides[index].classList.remove('active');
+
+        index++;
+        if (index === slides.length) index = 0;
+
+        slides[index].classList.add('active');
+
+    }, time);
+}
